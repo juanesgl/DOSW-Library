af64ad135f35425e452cd9265f35e6e5939ee638
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-03-17 17:29:39 -0500

    feature: Para que redireccione a API Swagger

---
+++ src/main/java/edu/eci/dosw/tdd/controller/IndexController.java
@@ -0,0 +1,15 @@
+package edu.eci.dosw.tdd.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Hidden
+public class IndexController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
